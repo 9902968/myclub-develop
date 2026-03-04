cb3622f0099b4384b35af33a9dde700fb787e397
Fuko <9902968@qq.com>
2026-03-05 00:42:59 +0800

    Initial commit

---
+++ src/main/java/com/ifuko/myclub/MyclubDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.ifuko.myclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyclubDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyclubDevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifuko/myclub/MyclubDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifuko.myclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyclubDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
